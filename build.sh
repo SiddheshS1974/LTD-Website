381e305d675fe91205e5957964f1fe002c4f0350
SiddheshS1974 <siddheshsk1706@gmail.com>
2026-05-24 13:21:16 -0400

    Set Admin role and name on superuser during build

--- build.sh
+++ build.sh
@@ -7,12 +7,18 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 import os
 User = get_user_model()
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', '')
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+first_name = os.environ.get('DJANGO_SUPERUSER_FIRST_NAME', '')
+last_name = os.environ.get('DJANGO_SUPERUSER_LAST_NAME', '')
 if username and password and not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print('Superuser created.')
+    u = User.objects.create_superuser(username=username, email=email, password=password)
+    u.role = 'Admin'
+    u.first_name = first_name
+    u.last_name = last_name
+    u.save()
+    print('Superuser created with Admin role.')
 else:
     print('Superuser already exists or credentials not set.')
 "
